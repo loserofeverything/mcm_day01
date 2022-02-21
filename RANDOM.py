4fe6b0b79b39408b58523eefcba6e1c2a70869d0
Xeon@DD <792299255@qq.com>
2022-02-21 14:50:35 +0800

    优化了随机数法的逻辑 并重置了其统计图片

--- RANDOM.py
+++ RANDOM.py
@@ -12,23 +12,15 @@ def getRandomWeight(mode):
         sums = np.sum(randlist)
         return [randlist[0]/sums, randlist[1]/sums]
 
-def Pump(dqy, mode ,mark,date):
+def Pump(dqy, mode ,mark, date):
     if mode == 0:
-        diff = dqy.money - mark
-        dqy.money = mark
-        return diff
+        pass
     elif mode == 1:
         dqy.sell_bitcoin(dqy.bitcoin_amount - mark/dqy.bitcoin_market[date], date)
-        diff = dqy.money - mark
-        dqy.money = mark
-        return diff
     elif mode == 2:
         dqy.sell_gold(dqy.gold_amount - mark/dqy.gold_market[date], date)
-        diff = dqy.money - mark
-        dqy.money = mark
-        return diff
 
-def Flood(dqy, mode ,mark, date, pool):
+def Flood(dqy, mode ,mark, date):
     if mode == 0:
         pass
     elif mode == 1:
@@ -45,43 +37,39 @@ def RANDOM(date, dqy, bitcoin, gold, time_sort):
         self_asset = np.sum(assetlist)
         Expections = getRandomWeight(0)
         Exp_list = self_asset * np.array([Expections[2],Expections[1],Expections[0] ])
-        money_pool = 0
         i = 0
         
         for Exp, actual in zip(Exp_list, assetlist):
             if i!= 2:
                 if actual > Exp:
-                    money_pool += Pump(dqy, i, Exp, date)
+                    Pump(dqy, i, Exp, date)
             else:
                 if actual > Exp:
-                    money_pool += Pump(dqy, i, Exp, gold_date_index)
+                    Pump(dqy, i, Exp, gold_date_index)
             i += 1
-        dqy.money += money_pool
         i = 0
         for Exp, actual in zip(Exp_list, assetlist):
             if i != 2:
                 if actual < Exp:
-                    Flood(dqy, i, Exp, date , money_pool)
+                    Flood(dqy, i, Exp, date)
             else:
                 if actual < Exp:
-                    Flood(dqy, i, Exp, gold_date_index , money_pool)
+                    Flood(dqy, i, Exp, gold_date_index)
             i += 1
     else:
         assetlist = [dqy.money, dqy.bitcoin_amount * bitcoin[date]]
         self_asset = np.sum(assetlist)
-        money_pool = 0
         i = 0
         Expections = getRandomWeight(1)
         Exp_list = self_asset * np.array([Expections[1], Expections[0]])
         for Exp, actual in zip(Exp_list, assetlist):
             if actual > Exp:
-                money_pool += Pump(dqy, i, Exp, date)
+                Pump(dqy, i, Exp, date)
             i += 1
-        dqy.money += money_pool
         i = 0
         for Exp, actual in zip(Exp_list, assetlist):
             if actual < Exp:
-                Flood(dqy, i, Exp, date , money_pool)
+                Flood(dqy, i, Exp, date)
             i += 1
         
                 
